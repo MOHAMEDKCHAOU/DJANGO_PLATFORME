04367096e3c1bb67a3f58771bf571ea0c6433bde
MOHAMEDKCHAOU <hamakch12@gmail.com>
2025-11-14 22:01:13 +0100

    Ajouter CRUD PRODUCT AND DASHBOARD FONCTIONS

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404,Sum, F
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Sum, F
 from .models import Product,StockMovement
 from .forms import ProductForm
 from django.shortcuts import render
