5ff08bc5ec3bdfc8e169b13f219288c2d54ed9b6
MOHAMEDKCHAOU <hamakch12@gmail.com>
2025-11-14 22:33:38 +0100

    add Category select Dynamique in form product

--- products/forms.py
+++ products/forms.py
@@ -5,6 +5,15 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'category', 'price', 'quantity', 'image']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+        }
+
 
 class CategoryForm(forms.ModelForm):
     class Meta:
