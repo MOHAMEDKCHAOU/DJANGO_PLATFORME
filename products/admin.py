e85ba44d4fa5e290d3978d3931310bc5adf631a9
MOHAMEDKCHAOU <hamakch12@gmail.com>
2025-11-19 20:28:05 +0100

    Reservation partie Crud

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 from .models import Category
 
 @admin.register(Product)
@@ -7,5 +7,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'quantity')
     search_fields = ('name', 'category')
     list_filter = ('category',)
-
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
+    
 admin.site.register(Category)
